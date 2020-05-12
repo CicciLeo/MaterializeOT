87da93acfca6119f0025eccc8149cd006f070eda
CicciLeo <58818751+CicciLeo@users.noreply.github.com>
2020-05-12 13:54:54 +0200

    Add files via upload

---
+++ js/core.js
@@ -0,0 +1,41 @@
+/*var back_to_top = document.getElementById("back-to-top");
+
+window.addEventListener('scroll', toggle_back_to_top);
+back_to_top.addEventListener('click', scrollToTop);*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);                    //options++
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems);
+});
+
+
+
+function toggle_back_to_top() {
+  if (window.pageYOffset > (window.innerHeight / 2) ) {
+    back_to_top.classList.remove("scale-out");
+    back_to_top.classList.add("scale-in");
+  } else {
+    back_to_top.classList.remove("scale-in");
+    back_to_top.classList.add("scale-out");
+  }
+}
+
+function scrollToTop () {
+  var c = window.pageYOffset;
+  if (c > 1) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  } else {
+    window.scrollTo(0, 0);
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,85 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);                    //options++
-  });
+window.addEventListener('load', adjust_navbar_and_padding);
+window.addEventListener('resize', adjust_navbar_and_padding);
+window.addEventListener('scroll', adjust_navbar);
 
-  document.addEventListener('DOMContentLoaded', function() {
-   var elems = document.querySelectorAll('.parallax');
-   var instances = M.Parallax.init(elems);                    //options++
- });
+function adjust_navbar_and_padding() {
+	var max_height = calc_max_height();
+	resize_logo(max_height, window.pageYOffset);
+	/*document.getElementsByTagName("main")[0].style.marginTop = max_height + "px";*/
+  document.getElementById("navbar-bordo").style.marginTop = (max_height - 75) + "px";
+}
 
-  window.onscroll = function () {
-		var y_offset = window.pageYOffset;
-    var real_height = calc_real_height();
+function adjust_navbar () {
+  resize_logo(calc_max_height(), window.pageYOffset);
+}
 
-    resize_logo(real_height, y_offset);
-/*		set_padding_height(real_height, y_offset);*/
+function calc_max_height () {
+	var max_height = window.innerWidth / 2.1;
+	return (max_height < 600) ? max_height : 600;
+}
 
-		toggle_back_to_top(y_offset);
-	}
-
-	window.onload = function () {
-		var y_offset = window.pageYOffset;
-		var real_height = calc_real_height();
-
-		resize_logo(real_height, y_offset);
-		set_padding_height(real_height, y_offset);
-
-		toggle_back_to_top(y_offset);
-	}
-
-	window.onresize = function () {
-		var y_offset = window.pageYOffset;
-		var real_height = calc_real_height();
-
-		resize_logo(real_height, y_offset);
-		set_padding_height(real_height, y_offset);
-	}
-
-	function calc_real_height () {
-		var real_height = window.innerWidth / 2.1;
-		return (real_height < 600) ? real_height : 600;
-	}
-
-	function resize_logo(real_height, y_offset) {
-		var dinamic_height = real_height - y_offset;
-		if (dinamic_height < 75) dinamic_height = 75;
-
-		document.getElementById("logo").style.maxHeight = dinamic_height +"px";
-    document.getElementById("logo").style.marginTop = real_height*0.01 +"px";
-    document.getElementById("navbar-content").style.height = dinamic_height +"px";
-    
-    console.log("dinamic_height: " + dinamic_height + "  --  real_height: " + real_height);
-	}
-
-	function set_padding_height(real_height, y_offset) {
-    var dinamic_height = real_height;
-
-		document.getElementsByTagName("main")[0].style.marginTop = dinamic_height + "px";
-	}
-
-
-  function toggle_back_to_top(y_offset) {
-    if (y_offset > 50) {
-      document.getElementById("back-to-top").classList.remove("scale-out");
-      document.getElementById("back-to-top").classList.add("scale-in");
-    } else {
-      document.getElementById("back-to-top").classList.remove("scale-in");
-      document.getElementById("back-to-top").classList.add("scale-out");
-    }
-  }
-
-  document.getElementById("back-to-top").onclick = function() {
-    console.log("bazinga");
-    window.requestAnimationFrame(scrollToTop);
-  }
-
-
-  function scrollToTop () {
-    var c = document.documentElement.scrollTop || document.body.scrollTop;
-    if (c > 0) {
-      window.requestAnimationFrame(scrollToTop);
-      c = (c < 1) ? 0 : c - c / 8;
-      window.scrollTo(0, c);
-    }
-  }
+function resize_logo(max_height, y_offset) {
+	var height = max_height - y_offset;
+	if (height < 75) height = 75;
+	document.getElementById("logo").style.maxHeight = height +"px";
+  document.getElementById("navbar-content").style.height = height +"px";
+}
