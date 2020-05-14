72246454302a26c2d9942027522be12a49f06488
CicciLeo <58818751+CicciLeo@users.noreply.github.com>
2020-05-15 01:45:23 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,22 +1,20 @@
 window.addEventListener('load', adjust_navbar_and_padding);
 window.addEventListener('resize', adjust_navbar_and_padding);
-window.addEventListener('scroll', adjust_navbar);
+/*window.addEventListener('scroll', adjust_navbar);*/
 
-document.getElementById("MANNAGGIALAPUTTANA").onscroll = function () {
-	console.log("MANNAGGIALAPUTTANA");
-	resize_logo(calc_max_height(), document.getElementById("MANNAGGIALAPUTTANA").scrollTop)
+document.getElementById("viewport").onscroll = function () {
+	resize_logo(calc_max_height(), document.getElementById("viewport").scrollTop);
 }
 
 
 function adjust_navbar_and_padding() {
 	var max_height = calc_max_height();
-	resize_logo(max_height, window.pageYOffset);
-  /*document.getElementById("navbar-bordo").style.marginTop = (max_height - 75) + "px";*/
+	resize_logo(max_height, document.getElementById("viewport").scrollTop);
 }
 
-function adjust_navbar () {
+/*function adjust_navbar () {
   resize_logo(calc_max_height(), window.pageYOffset);
-}
+}*/
 
 function calc_max_height () {
 	var max_height = window.innerWidth / 2.1;
@@ -24,7 +22,7 @@ function calc_max_height () {
 }
 
 function resize_logo(max_height, y_offset) {
-	var height = max_height - y_offset;
+	var height = (y_offset >= 0) ? (max_height - y_offset) : (max_height);
 	if (height < 75) height = 75;
 	document.getElementById("logo").style.maxHeight = height +"px";
   document.getElementById("navbar-content").style.height = height +"px";
